67698bc2d12e9a591ffa9366569492f7da4ab81a
Muluken Zeleke <mulukenzeleke638@gmail.com>
2024-10-28 23:27:37 +0000

    Recommended is appeared

---
+++ src/Components/Recomended/RecomendedFilms.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RecomendedFilms() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/HomePage.js
+++ src/HomePage.js
@@ -5,12 +5,28 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination } from "swiper/modules";
+import RecommendedMovies from "./Components/REcomended/RecommendedMovies";
 
 const Homepage = () => {
   const movies = [
-    { id: 1, title: "Movie 1", description: "Exciting movie 1", image: "/path-to-image1.jpg" },
-    { id: 2, title: "Movie 2", description: "Amazing movie 2", image: "/path-to-image2.jpg" },
-    { id: 3, title: "Movie 3", description: "Thrilling movie 3", image: "/path-to-image3.jpg" },
+    {
+      id: 1,
+      title: "Movie 1",
+      description: "Exciting movie 1",
+      image: "/path-to-image1.jpg",
+    },
+    {
+      id: 2,
+      title: "Movie 2",
+      description: "Amazing movie 2",
+      image: "/path-to-image2.jpg",
+    },
+    {
+      id: 3,
+      title: "Movie 3",
+      description: "Thrilling movie 3",
+      image: "/path-to-image3.jpg",
+    },
     // Add more movies
   ];
 
@@ -22,7 +38,7 @@ const Homepage = () => {
   ];
 
   return (
-    <div>
+    <div className="p-0 m-0">
       {/* Swiper for Featured Movies */}
       <Swiper
         modules={[Navigation, Pagination]}
@@ -34,7 +50,11 @@ const Homepage = () => {
         {movies.map((movie) => (
           <SwiperSlide key={movie.id}>
             <div className="relative w-full h-96">
-              <img src={movie.image} alt={movie.title} className="object-cover w-full h-full" />
+              <img
+                src={movie.image}
+                alt={movie.title}
+                className="object-cover w-full h-full"
+              />
               <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-center text-white px-4">
                 <h2 className="text-3xl font-bold">{movie.title}</h2>
                 <p className="text-lg mt-2">{movie.description}</p>
@@ -45,17 +65,7 @@ const Homepage = () => {
       </Swiper>
 
       {/* Recommended Movies Section */}
-      <section className="my-8">
-        <h3 className="text-2xl font-semibold mb-4">Recommended Movies</h3>
-        <div className="flex space-x-4 overflow-x-scroll pb-4">
-          {recommendedMovies.map((movie) => (
-            <div key={movie.id} className="min-w-[200px] h-60 flex-shrink-0">
-              <img src={movie.image} alt={movie.title} className="w-full h-full object-cover rounded-lg" />
-              <h4 className="text-center text-white mt-2">{movie.title}</h4>
-            </div>
-          ))}
-        </div>
-      </section>
+      <RecommendedMovies />
     </div>
   );
 };
